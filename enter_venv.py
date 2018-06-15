fba729e32b22917f6af8575cb9ef2ce0ff542581
Ashfaq Rahman <ashrmzn22@gmail.com>
2018-06-15 18:28:27 +0530

    added auto switch to src directory

--- enter_venv.py
+++ enter_venv.py
@@ -25,14 +25,29 @@ def get_files(list_of_dirs, prev=False):
 search_lists = [os.listdir(), os.listdir('../'), get_files(os.listdir()), get_files(os.listdir('../'), True)]
 
 target=None
+src_target=None
+target_found=False
 break_again=False
+src_found=False
 for index,each_search_list in enumerate(search_lists):
     for index2,path in enumerate(each_search_list):
         if path.split('/')[-1] == 'activate':
-            target=index,index2
-            break_again=True
-            break;
+            target=index, index2
+            target_found=True
+
+        if path.split('/')[-1] == 'src':
+            src_target=index, index2
+            src_found = True
+
+        if target_found and src_found:
+            break_again = True
+            break
     if break_again: break
+
+try:
+    os.chdir(os.path.abspath(search_lists[src_target[0]][src_target[1]]))
+except:
+    print('unable to switch to root directory of django, root directory must be named as "src".')
 
 try:
     activate_path=search_lists[target[0]][target[1]]
