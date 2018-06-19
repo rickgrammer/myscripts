591dcf2c31ce2286afc27280f6ec76d53a05c71f
Ashfaq Rahman <ashrmzn22@gmail.com>
2018-06-19 13:14:57 +0530

    added collectstatic shortcut

--- python-manage.py
+++ python-manage.py
@@ -8,7 +8,7 @@ shortcuts={'r': 'runserver',
         'c': 'check',
         'mk': 'makemigrations',
         'm': 'migrate',
-        'c': 'check',
+        'cs': 'collectstatic',
         'sa': 'startapp',
         }
 
